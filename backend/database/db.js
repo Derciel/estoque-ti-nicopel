e341c8d66b4f6be2a99792e7e78e414497a630b2
Derciel <dercieljr2@gmail.com>
2026-01-14 15:55:21 -0300

    correção do db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,4 +1,13 @@
 const knex = require('knex');
-const config = require('../knexfile.js');
-const db = knex(config.development);
+const path = require('path');
+
+const db = knex({
+    client: 'sqlite3',
+    connection: {
+        // Se existir a variável do Render, usa ela. Senão, usa o caminho local.
+        filename: process.env.DB_CONNECTION_FILENAME || path.resolve(__dirname, '../dev.sqlite3'),
+    },
+    useNullAsDefault: true,
+});
+
 module.exports = db;
